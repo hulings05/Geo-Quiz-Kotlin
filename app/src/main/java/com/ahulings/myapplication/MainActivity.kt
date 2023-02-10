c642f1c64b64454d1026ded49cae1d3e25978a5b
hulings05 <amh361@pitt.edu>
2023-02-09 19:45:43 -0500

    page 94

---
+++ app/src/main/java/com/ahulings/myapplication/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.ahulings.myapplication
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import com.ahulings.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(this,R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        }
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(this,R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
